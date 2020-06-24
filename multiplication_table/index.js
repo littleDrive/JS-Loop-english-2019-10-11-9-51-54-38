fda56b82954b15273b0a34fc6456ca9db77f8596
小司机 <36127072+littleDrive@users.noreply.github.com>
2020-06-24 23:00:45 +0800

    Update index.js

--- multiplication_table/index.js
+++ multiplication_table/index.js
@@ -1,1 +1,10 @@
 
+let temp;
+for (let i = 1; i < 10; i++) {
+    temp = " ";
+    result = " ";
+    for (let j = 1; j <= i; j++) {
+        temp += i + "*" + j + " = " + i * j + " "; 
+    }
+    console.log(temp);
+}
